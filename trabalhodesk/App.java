021c569991ca2ccd55f144187e67a6a2c8fe4e74
Cleiton Aparecido Ramalho <130614000+cleiton4513@users.noreply.github.com>
2025-04-12 21:23:53 -0300

    Add files via upload

---
+++ trabalhodesk/App.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.trabalhodesk;
+
+/**
+ *
+ * @author cleit
+ */
+public class App {
+
+    public static void main(String[] args) {
+        
+        new LoginFrame().setVisible(true);
+        //new MecanicaFrame().setVisible(true);
+    }
+}
